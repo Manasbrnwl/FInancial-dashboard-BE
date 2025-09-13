1e5ad1d801a4b5dca89a4e458ebb7dc2796a983b
Manas Baranwal <72220768+Manasbrnwl@users.noreply.github.com>
2025-09-13 11:51:29 +0530

    loadEnv updated

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,21 +1,27 @@
 import { config } from "dotenv";
 
-
 export const loadEnv = () => {
   config();
-  
+
   // Required environment variables
   const requiredEnvVars = [
-    'PORT',
-    'API_USERNAME',
-    'API_PASSWORD',
-    'LOGIN_API_URL'
+    "PORT",
+    "API_USERNAME",
+    "API_PASSWORD",
+    "LOGIN_API_URL",
+    "EMAIL_USER",
+    "EMAIL_PASS",
+    "RECEIVER_EMAIL",
   ];
-  
+
   // Check if all required environment variables are set
-  const missingEnvVars = requiredEnvVars.filter(envVar => !process.env[envVar]);
-  
+  const missingEnvVars = requiredEnvVars.filter(
+    (envVar) => !process.env[envVar]
+  );
+
   if (missingEnvVars.length > 0) {
-    throw new Error(`❌ Missing required environment variables: ${missingEnvVars.join(', ')}`);
+    throw new Error(
+      `❌ Missing required environment variables: ${missingEnvVars.join(", ")}`
+    );
   }
 };

718f3d16e79e660dcab2fbabbb0efc168048bc10
Manas Baranwal <72220768+Manasbrnwl@users.noreply.github.com>
2025-11-24 12:05:15 +0530

    domain updated

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ socketIOService.initialize(httpServer);
 // CORS configuration - allow requests from frontend
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://15.207.43.160:8080", "http://anfy.in:8080", "http://anfy.in", "https://anfy.in"],
+    origin: ["https://anfy.in", "https://www.anfy.in", "anfy.in", "www.anfy.in", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
